a023f1870e1a4467d97993f49df31c0bc13ebe60
Angeljcc102 <angeljcc102@outlook.es>
2019-04-05 02:45:14 +0000

    Proyecto Parte 1

--- proc.c
+++ proc.c
@@ -322,8 +322,10 @@ void
 scheduler(void)
 {
   struct proc *p;
+	struct proc *hp;
   struct cpu *c = mycpu();
   c->proc = 0;
+	hp = 0;
   
   for(;;){
     // Enable interrupts on this processor.
@@ -334,6 +336,15 @@ scheduler(void)
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
+	  if(hp == 0){
+		  hp = p;
+	  } else {
+		  if(hp->prio <= p->prio){
+			  hp = p;
+		  }
+	  }
+	}
+	  p = hp;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
@@ -348,10 +359,8 @@ scheduler(void)
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
-    }
     release(&ptable.lock);
-
-  }
+	}
 }
 // Enter scheduler.  Must hold only ptable.lock
 // and have changed proc->state. Saves and restores
